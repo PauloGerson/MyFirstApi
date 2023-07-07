1020bf3a8a9aea924b406b7467166f389456f1a7
PauloGerson <35962308+PauloGerson@users.noreply.github.com>
2023-07-07 03:31:13 -0300

    Rota de post

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const { v4: uuid } = require("uuid");
 
 const app = express();
-
-app.listen(5500, () => console.log("Rodando na porta 5500"));
 
 app.use(cors());
 
@@ -11,14 +10,28 @@ app.use(express.json());
 
 const produtos = [
   {
-    id: 123,
+    id: uuid(),
     name: "paulo",
     password: "123",
   },
 ];
 
-app.get("/api", (req, res) =>
-  res.json({
-    produtos,
-  })
-);
+app.get("/api", (request, response, next) => {
+  response.status(200).json(produtos);
+});
+
+app.post("/api", (request, response, next) => {
+  const { name, password } = request.body;
+
+  const data = {
+    id: uuid(),
+    name,
+    password,
+  };
+
+  produtos.push(data);
+
+  response.json({ message: "Salvo" });
+});
+
+app.listen(3002, () => console.log("Rodando na porta 3002"));
--- frontend/script.js
+++ frontend/script.js
@@ -3,22 +3,28 @@ const formLogin = document.querySelector("#loginForm");
 formLogin.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  const userName = document.querySelector("#usernameInput").value;
+  let userName1 = document.querySelector("#usernameInput").value;
   const password1 = document.querySelector("#passwordInput").value;
 
-  const url = "http://localhost:5500/api";
+  const url = "http://localhost:3002/api";
 
   fetch(url)
     .then((data) => data.json())
     .then((resorce) => {
-      const dados = resorce.produtos.find(
-        (resorces) =>
-          resorces.name === userName && resorces.password === password1
-      );
+      //console.log(resorce);
+
+      /* const dados = resorce.produtos.find(
+        (dados) => dados.name === userName1 && dados.password === password1
+      ); */
+
+      const dados = resorce;
+
       if (dados) {
         console.log(dados);
       } else {
         console.log("Dados incorretos");
       }
+
+      document.querySelector("#usernameInput").value = ""; // Limpa o valor do campo de entrada
     });
 });
